4d8cecbafbb351b4e9971b4e9d3d9b5e113200e9
axlrommel <rommelvillagomez77@gmail.com>
2021-10-06 23:39:28 -0500

    updates

---
+++ functionChallenges/doubleTheNumber.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+function doubleTheNumber (arr) {
+  if(arr === undefined)
+    return undefined;
+	return arr.map(i => i * 2);
+}
+
+assert.deepEqual(doubleTheNumber(), undefined);
+assert.deepEqual(doubleTheNumber([]), []); 
+assert.deepEqual(doubleTheNumber([4]), [8]);
+assert.deepEqual(doubleTheNumber([1, 3, 5]), [2, 6, 10]);
+assert.deepEqual(doubleTheNumber([1, 2, 5]), [2, 4, 10]);
+console.log('all tests passed!');
--- functionChallenges/evenNumbersInArray.js
+++ functionChallenges/evenNumbersInArray.js
@@ -1,19 +1,12 @@
 const assert = require('assert');
 
-function evenNumbersinArray (arr) {
-  if(arr === undefined)
-    return undefined;
-	let evens = [];
-	for(let i = 0; i < arr.length; i++) {
-		if(arr[i] % 2 === 0) {
-      evens.push(arr[i]);
-    }
-	}
-	return evens;
+function evenNumbersinArray(arr) {
+	if (arr === undefined) return undefined;
+	return arr.filter(i => i % 2 === 0);
 }
 
 assert.deepEqual(evenNumbersinArray(), undefined);
-assert.deepEqual(evenNumbersinArray([]), []); 
+assert.deepEqual(evenNumbersinArray([]), []);
 assert.deepEqual(evenNumbersinArray([4]), [4]);
 assert.deepEqual(evenNumbersinArray([1, 3, 5]), []);
 assert.deepEqual(evenNumbersinArray([1, 2, 5]), [2]);
